8cb767f64880e08d3105eeb2027a0da26dae07fb
mjrdnk <maciej.jordanek@gmail.com>
2021-04-06 15:51:16 +0100

    refactor to send response code after successful image download

--- src/handlers/handleScreenshot.ts
+++ src/handlers/handleScreenshot.ts
@@ -1,28 +1,18 @@
 import { Request, Response } from "express";
-import playwright from "playwright";
-import { Browsers } from "../enum";
+import { takeScreenshots } from "../helpers";
 
 export default async function (req: Request, res: Response) {
-  const { url } = req.query;
+  const { url, retake } = req.query;
   if (url == null) {
-    res.send(400);
+    res.sendStatus(400);
   }
 
   // if it's array, get the first element, otherwise take the string
   const parsedUrl = Array.isArray(url) ? url[0] : url;
-
-  if (parsedUrl) {
-    for (const browserType of [
-      Browsers.CHROMIUM,
-      Browsers.FIREFOX,
-      Browsers.WEBKIT,
-    ]) {
-      const browser = await playwright[browserType].launch();
-      const context = await browser.newContext();
-      const page = await context.newPage();
-      await page.goto(parsedUrl as string);
-      await page.screenshot({ path: `downloads/${browserType}.png` });
-      await browser.close();
-    }
+  const shots = await takeScreenshots(parsedUrl as string, Boolean(retake));
+  if (shots) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(500);
   }
 }
---
+++ src/helpers.ts
@@ -0,0 +1,29 @@
+import playwright from "playwright";
+import { Browsers } from "./enum";
+
+export async function takeScreenshots(
+  // url of the page to screenshot
+  parsedUrl: string,
+  // puts new images in the /new/ directory
+  isNew: boolean = false
+) {
+  const promises = [Browsers.CHROMIUM, Browsers.FIREFOX, Browsers.WEBKIT].map(
+    async (browserType) => {
+      try {
+        const browser = await playwright[browserType].launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto(parsedUrl as string);
+        const shot = await page.screenshot({
+          path: `downloads/${isNew ? "new/" : ""}${browserType}.png`,
+        });
+        await browser.close();
+        return shot;
+      } catch (error) {
+        throw error;
+      }
+    }
+  );
+
+  return Promise.all(promises);
+}
